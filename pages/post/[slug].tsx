5d60c307569b231be6e09bd41fdbf2ef58436477
zilla <samyusuf101@gmail.com>
2022-08-17 01:20:43 -0700

    updated slug.tsx

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -37,21 +37,33 @@ function Post({ post }: Props) {
           </p>
         </div>
 
-        <div>
-            <PortableText
-            className=""
-            dataset = {process.env.NEXT_PUBLIC_SANITY_DATASET!}
-            projectId = {process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+        <div className="mt-10">
+          <PortableText
+            className="text-black"
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
             content={post.body}
-            serializers={
-              h1: (props: any) => {
-                <h1 className="text-2xl font-bold my-5" {...props} />
-              }
-            }
-                     
-            />
+            serializers={{
+              h1: (props: any) => (
+                <h1 className="text-2xl  font-bold my-5" {...props} />
+              ),
+              h2: (props: any) => (
+                <h1 className="text-xl  font-bold my-5" {...props} />
+              ),
+              li: ({ children }: any) => (
+                <li className="ml-4  list-disc">{children}</li>
+              ),
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>
+              ),
+            }}
+          />
         </div>
       </article>
+
+      <hr className="" />
     </main>
   );
 }
