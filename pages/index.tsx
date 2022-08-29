2a6dc5fca42e091adfbc96ea5e42af07a3109436
Samyusuf101 <samyusuf101@gmail.com>
2022-08-29 04:10:51 -0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home({ posts }: Props) {
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
-        <title>Sa blog</title>
+        <title>Sam's blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
